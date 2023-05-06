5fee15117b218e0d49d398aa99e1a31d6f4b2f2b
Harshit <harshit.owo@gmail.com>
2023-05-06 09:45:06 +0530

    added slider class

--- sketch.js
+++ sketch.js
@@ -1,11 +1,13 @@
 var cnv
 var scaleCanvas
-
+const mySliders = []
 function setup() {
   cnv = createCanvas(scaleCanvas * dim, scaleCanvas * dim);
   background(bgc);
   changeDim()
   cnv.parent('canvas-div')
+  s = new Slider('Velocity', 'slider-ol')
+  s.addSlider(0, 100, 50)
 }
 function windowResized() {
   changeDim()
---
+++ slider.class.js
@@ -0,0 +1,18 @@
+
+class Slider {
+    constructor(name, formid) {
+
+        this.name = name
+        this.formid = formid
+    }
+    addSlider(min, max, value, step = 0.1) {
+        this.parentList = createElement('li')
+        this.parentList.parent(this.formid)
+
+        this.element = createSlider(min, max, value, step)
+        const p = createP(this.name)
+        this.parentList.child(this.element)
+        this.parentList.child(p)
+
+    }
+}
