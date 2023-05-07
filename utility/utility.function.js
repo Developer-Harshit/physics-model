1c12c6e065099cbff53a8af908cd6fe360b1a484
Harshit <harshit.owo@gmail.com>
2023-05-07 13:06:31 +0530

    made slider responsive for mobile

--- utility/utility.function.js
+++ utility/utility.function.js
@@ -12,8 +12,8 @@ var accx
 var accy
 
 function setSliders() {
-    accx = new Slider('acc-x', 'slider-ol')
-    accy = new Slider('acc-y', 'slider-ol')
+    accx = new Slider('Acceleration-X', 'slider-ol')
+    accy = new Slider('Acceleration-Y', 'slider-ol')
 
     accx.addSlider(-5.0, 5.0, 0, 0.0001)
     accy.addSlider(-5.0, 5.0, 0, 0.0001)
@@ -21,14 +21,24 @@ function setSliders() {
     mySliders.accx = accx.element
     mySliders.accy = accy.element
 
-    velx = new Slider('vel-x', 'slider-ol')
-    vely = new Slider('vel-y', 'slider-ol')
+    velx = new Slider('Velocity-X', 'slider-ol')
+    vely = new Slider('Velocity-Y', 'slider-ol')
 
     velx.addSlider(-20, 20, 0, 0.01)
     vely.addSlider(-20, 20, 0, 0.01)
 
     mySliders.velx = velx.element
     mySliders.vely = vely.element
+
+    s1 = new Slider('Slider-X', 'slider-ol')
+    s2 = new Slider('Slider-Y', 'slider-ol')
+
+    s1.addSlider(-20, 20, 0, 0.01)
+    s2.addSlider(-20, 20, 0, 0.01)
+
+
+
+
 
 
 }
